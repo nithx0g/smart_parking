6243f94cb69a34f6d2a4ef65f350a4c5001e076c
nithx0g <nitheeshgundapu@gmail.com>
2021-11-12 16:58:33 +0530

    update

--- sim.py
+++ sim.py
@@ -9,12 +9,12 @@ slots = {}
 occupied = []
 free = []
 sensors = {}
-k=1
+random_var=1
 for i in range(1,21):
     slots[i] = "" #4-digit car number if parked there
-    if(i%8 == k):
+    if(i%8 == random_var):
         slots[i] = str(random.randint(1111,9999))
-        k=(2*k+3)%4 + 1
+        random_var=(2*random_var+3)%4 + 1
         occupied.append(i)
     else:
         free.append(i)
@@ -22,14 +22,14 @@ for i in range(1,21):
 serv = server.Server(slots)
 camera = camera.Camera()
 time.sleep(2)
-
 list_slot = list(serv.slots.values())
-
+# main window 
 par = Tk()
-
+# title of parking slot
 par.title("Smart Parking Simulation")
 code_entry=Entry(par,width=20,borderwidth=5)
 code_entry.grid(row=1,column=1)
+#entryclick used to get the entry of car
 def entryclick():
     if(len(free) == 0):
         Label(par ,width=20,height=3,text="Parking space is full").grid(row  = 6,column =0)
@@ -45,7 +45,7 @@ def entryclick():
     occupied.append(free_slot)
     print("Parked at " + str(free_slot))
     sensors[free_slot].entry()
-  
+#use this fun to get car to exit from the parking lot  
 code_exit=Entry(par,width=20,borderwidth=5)
 code_exit.grid(row=4,column=1)
 def exitclick():
@@ -63,24 +63,22 @@ def exitclick():
     occupied.remove(exiting_slot)
     free.append(i)
     sensors[exiting_slot].exit()
-    
 
 Label(par ,width=15,height=3,text="Entry Car:").grid(row  = 1,column =0)
-
 Button(par ,text="Entry",activebackground="Black",command=entryclick).grid(row=1,column=2)
 Label(par ,width=15,height=3,text="").grid(row  = 2,column =0)
 Label(par ,width=15,height=3,text="Exit Car:").grid(row  = 4,column =0)
 Button(par ,text="Exit",activebackground="Black",command=exitclick).grid(row=4,column=2)
 
-
-
+# sub window for user 
 user = Tk()
-
 user.title("USER UI")
-yy = []
+car_location = []
+# search car location from car number
 Label(user ,width=20,height=3,text="Search Car Location").grid(row=1)
 code=Entry(user,width=20,borderwidth=5)
 code.grid(row=2,column=0)
+# slot fun use for searching car location
 def slot():
     list_slot = list(serv.slots.values())
     val = code.get()
@@ -88,76 +86,74 @@ def slot():
         Label(user ,width=15,height=3,text="No input given").grid(row=4,column=0,padx=10,pady=10)
         return
     slot_pos=0
-    for i in range(len(yy)):
+    for i in range(len(car_location)):
         Label(user ,width=15,height=3,text="").grid(row=4,column=i,padx=10,pady=10)
-    yy.clear()
+    car_location.clear()
     while True:
         try:
             slot_pos = list_slot.index(val, slot_pos)
-            yy.append(slot_pos+1)
+            car_location.append(slot_pos+1)
             slot_pos += 1
         except ValueError as e:
             break
-    for i in range(len(yy)):
-        Label(user ,width=15,height=3,text="Car Location:{}".format(yy[i]),bg="#CDC0B0").grid(row=4,column=i,padx=10,pady=10)
-    if(len(yy) == 0):
+    for i in range(len(car_location)):
+        Label(user ,width=15,height=3,text="Car Location:{}".format(car_location[i]),bg="#CDC0B0").grid(row=4,column=i,padx=10,pady=10)
+    if(len(car_location) == 0):
         Label(user ,width=15,height=3,text="Car not found").grid(row=4,column=0,padx=10,pady=10)
 
-
-Button(user ,width=5,text="Search",activebackground="Black",font="Raleway",bg="#20bebe",command=slot).grid(row=2,column=1)
+# car serach button calls slot function 
+Button(user ,width=5,text="Car Search",activebackground="Black",font="Raleway",bg="#20bebe",command=slot).grid(row=2,column=1)
 Label(user ,width=15,height=3,text="Slots").grid(row=5) 
  
-
+# here we dispaly all parking slot info
 count=0
-k=0
+row_in=0
 for i in list_slot: 
     if(i==""):
-        Label(user ,width=15,height=3,text="free",bg="blue").grid(row=6+k,column=count,padx=10,pady=10)
+        Label(user ,width=15,height=3,text="free",bg="blue").grid(row=6+row_inc,column=count,padx=10,pady=10)
         count=count+1
         if(count==4):
-            k=k+1
+            row_inc=row_inc+1
             count=0
     else:
-        Label(user ,width=15,height=3,text=i,bg="red").grid(row=6+k,column=count,padx=10,pady=10)
+        Label(user ,width=15,height=3,text=i,bg="red").grid(row=6+row_inc,column=count,padx=10,pady=10)
         count=count+1
         if(count==4):
-            k=k+1
+            row_inc=row_inc+1
             count=0
 frees=[]
 Label(user ,width=15,height=3,text="find free slot").grid(row=14,column=0)
-
+# Update fun update ui page
 def update():
     count=0
-    k=0
+    row_in=0
     list_slot = list(serv.slots.values())
     for i in list_slot: 
         if(i==""):
-            Label(user ,width=15,height=3,text="free",bg="blue").grid(row=6+k,column=count,padx=10,pady=10)
+            Label(user ,width=15,height=3,text="free",bg="blue").grid(row=6+row_in,column=count,padx=10,pady=10)
             count=count+1
             if(count==4):
-                k=k+1
+                row_in=row_in+1
                 count=0
         else:
-            Label(user ,width=15,height=3,text=i,bg="red").grid(row=6+k,column=count,padx=10,pady=10)
+            Label(user ,width=15,height=3,text=i,bg="red").grid(row=6+row_in,column=count,padx=10,pady=10)
             count=count+1
             if(count==4):
-                k=k+1
+                row_in=row_in+1
                 count=0
-    # frees=[]
-# Label(user ,width=15,height=3,text="find free slot").grid(row=14,column=0)
-j=0
+# freeslot find free slot available in a parking slot                
+row_val=0
 def freeslot():
-    j=0
+    row_val=0
     count = 0
     list_slot = list(serv.slots.values())
     val = ""
     slot_pos=0
     for i in range(len(frees)):
-        # Label(user ,width=10,height=3,text="").grid(row=4,column=i,padx=5,pady=5)
-        Label(user ,width=10,height=3,text="").grid(row=16+j,column=count,padx=10,pady=10)
+        Label(user ,width=10,height=3,text="").grid(row=16+row_val,column=count,padx=10,pady=10)
         count = count + 1
         if(count==4):
-            j = j+1
+            row_val = row_val+1
             count=0
     frees.clear()
     while True:
@@ -167,25 +163,20 @@ def freeslot():
             slot_pos += 1
         except ValueError as e:
             break
-    j=0
+    row_val=0
     count = 0
     for i in range(len(frees)):
-        # if(count == 4):
-        Label(user ,width=10,height=3,text="{}".format(frees[i]),bg="#CDC0B0").grid(row=16+j,column=count,padx=10,pady=10)
+        Label(user ,width=10,height=3,text="{}".format(frees[i]),bg="#CDC0B0").grid(row=16+row_val,column=count,padx=10,pady=10)
         count = count + 1
         if(count==4):
-            j = j+1
+            row_val = row_val+1
             count=0
     
-
-
-Button(user ,width=5,text="Search",activebackground="Black",font="Raleway",bg="#20bebe",command=freeslot).grid(row=14,column=1)
-Button(user ,width=5,text="Update",activebackground="Black",font="Raleway",bg="#20bebe",command=update).grid(row=15,column=1)       
+#FreeSlot button calls the freeslot fun
+Button(user ,width=5,text="FreeSlot",activebackground="Black",font="Raleway",bg="#20bebe",command=freeslot).grid(row=14,column=1)
+#UpdateUI button calls the Update fun 
+Button(user ,width=5,text="UpdateUI",activebackground="Black",font="Raleway",bg="#20bebe",command=update).grid(row=15,column=1)       
+# user window end
 user.mainloop()
-
+# main window end
 par.mainloop()
-
-# root = Tk()
-# root.title("USer")
-# Label(root ,width=50,height=3,text="USer").grid(row=0)
-# root.mainloop()
